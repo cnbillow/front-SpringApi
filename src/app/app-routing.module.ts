3cdda4855ad3cb6a804e043550708968d2491635
Hugo Queiroz <hugoqueiroz@Hugos-MacBook-Pro.local>
2018-08-25 17:42:13 -0700

    routes configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,23 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule }        from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
+import { HomeComponent }     from './home/home.component';
+import { LoginComponent }    from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+
+const appRoutes: Routes = [
+      { path: '', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+];
 
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+  // providers: [
+  //   LoginService
+  // ]
 })
 export class AppRoutingModule { }
